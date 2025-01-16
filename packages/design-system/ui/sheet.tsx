132d4de34da6e33f416b704b8da5d03306b3bc12
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-01-16 03:04:28 +0000

    fix(design-system): minor ui issue in the sheet component

--- packages/design-system/ui/sheet.tsx
+++ packages/design-system/ui/sheet.tsx
@@ -153,7 +153,7 @@ const SheetContent = React.forwardRef<
             <SheetPrimitive.Close
               className={cn(
                 "absolute right-4 native:-mt-5 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary",
-                "bottom" === side ? "pt-6" : "pt-safe-offset-1",
+                "bottom" === side ? "native:pt-6" : "native:pt-safe-offset-1",
               )}
             >
               <X className="h-4 w-4 text-foreground" />
