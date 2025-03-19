a90366096fba851842ab75ee403dc8115fa165d9
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-03-19 21:08:16 +0000

    feat(design-system): improved layout UI

--- packages/design-system/layout/index.tsx
+++ packages/design-system/layout/index.tsx
@@ -1,13 +1,8 @@
 import { ActivityIndicator, ColorValue, Platform, View } from "react-native";
+import Animated, { SharedValue } from "react-native-reanimated";
 import { StatusBar, StatusBarProps } from "expo-status-bar";
 import * as NavigationBar from "expo-navigation-bar";
 import React from "react";
-import Animated, {
-  AnimatedProps,
-  FadeIn,
-  FadeOut,
-  SharedValue,
-} from "react-native-reanimated";
 
 import { useAfterInteractions } from "../hooks/useAfterInteraction";
 import { cn } from "../lib/utils";
@@ -29,11 +24,6 @@ export type LayoutProps = Omit<
   className?: string;
   wait?: boolean;
   delay?: number | false;
-  animated?: {
-    className?: string;
-    entering?: AnimatedProps<View>["entering"];
-    exiting?: AnimatedProps<View>["exiting"];
-  };
 };
 
 export function ScreenLayout({
@@ -43,24 +33,17 @@ export function ScreenLayout({
   className,
   status,
   wait,
-  animated,
   delay = false,
   ...props
 }: LayoutProps) {
-  const DefaultPlaceHolder = React.useCallback(
-    () => (
-      <Animated.View
-        className={cn(
-          "bg-background h-full w-full justify-center pb-safe",
-          animated?.className,
-        )}
-        entering={FadeIn.duration(500)}
-        exiting={FadeOut}
-      >
-        <ActivityIndicator size="large" />
-      </Animated.View>
-    ),
-    [animated?.className],
+  const DefaultPlaceHolder = () => (
+    <Animated.View
+      className={cn("bg-background h-full w-full justify-center pb-safe")}
+      entering={props?.entering}
+      exiting={props?.exiting}
+    >
+      <ActivityIndicator size="large" />
+    </Animated.View>
   );
 
   const { transitionRef, areInteractionsComplete } = useAfterInteractions<Animated.View>(
@@ -84,18 +67,12 @@ export function ScreenLayout({
   return (
     <View
       className={cn(
-        "bg-background flex-grow py-safe px-safe-offset-4 ios:landscape:px-safe-offset-1 transition-all android:duration-300",
+        "bg-background flex-grow pb-safe pt-safe-offset-2 px-safe-offset-4 transition-all android:duration-300",
         className,
       )}
     >
       <StatusBar style="auto" {...status} />
-      <Animated.View
-        entering={animated?.entering}
-        exiting={animated?.exiting}
-        ref={transitionRef}
-        style={{ flex: 1 }}
-        {...props}
-      >
+      <Animated.View ref={transitionRef} style={{ flex: 1 }} {...props}>
         {areInteractionsComplete ? (
           typeof children === "function" ? (
             children(areInteractionsComplete, Placeholder ?? DefaultPlaceHolder)
