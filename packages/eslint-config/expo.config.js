c0b7dc9b43cd4130106a4c2e85ff11461e0997f1
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-04-20 19:37:40 +0000

    feat(eslint-config): updated expo to flat config

--- packages/eslint-config/expo.config.js
+++ packages/eslint-config/expo.config.js
@@ -1,13 +1,15 @@
-// https://docs.expo.dev/guides/using-eslint/
+const pluginPrettier = require("eslint-config-prettier/flat");
+const pluginExpo = require("eslint-config-expo/flat");
+const { defineConfig } = require("eslint/config");
 
-/** @type {import('eslint').Linter.Config[]} */
-module.exports = [
+/** https://docs.expo.dev/guides/using-eslint/ */
+module.exports = defineConfig([
+    pluginPrettier,
+    pluginExpo,
     {
-        plugins: {
-            expo: require("eslint-config-expo"),
-        },
         rules: {
+            "react-hooks/exhaustive-deps": "off",
             "@typescript-eslint/no-unused-vars": "off",
         },
     },
-];
+]);
