1b83c3e4e6ef29872dd79e3e513b76169dad6b9c
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-06-09 09:27:50 +0000

    feat(design-system): added ripple button UI component

--- packages/design-system/ui/button.tsx
+++ packages/design-system/ui/button.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { Animated, GestureResponderEvent, Pressable as RNPressable } from "react-native";
 import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react";
-import { Pressable as RNPressable } from "react-native";
 import { Pressable } from "@rn-primitives/slot";
+import * as React from "react";
+
 import { TextClassContext } from "./text";
 import { cn } from "../lib/utils";
 import isWeb from "../lib/isWeb";
 
 const buttonVariants = cva(
-  "group flex items-center justify-center rounded-md web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
+  "group flex items-center justify-center rounded-md web:ring-offset-background active:scale-95 web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
@@ -67,11 +68,12 @@ type ButtonProps = React.ComponentPropsWithoutRef<typeof RNPressable> &
     /** Web only */
     type?: "submit" | "reset" | "button";
     asChild?: boolean;
+    disableRipple?: boolean;
   };
 
 const Button = React.forwardRef<React.ComponentRef<typeof RNPressable>, ButtonProps>(
-  ({ className, variant, size, asChild, ...props }, ref) => {
-    const Component = asChild && isWeb ? Pressable : RNPressable;
+  ({ className, variant, size, asChild, disableRipple, ...props }, ref) => {
+    const Btn = asChild && isWeb ? Pressable : disableRipple ? RNPressable : RippleButton;
     return (
       <TextClassContext.Provider
         value={cn(
@@ -79,7 +81,7 @@ const Button = React.forwardRef<React.ComponentRef<typeof RNPressable>, ButtonPr
           buttonTextVariants({ variant, size }),
         )}
       >
-        <Component
+        <Btn
           className={cn(
             props.disabled && "opacity-50 web:pointer-events-none",
             buttonVariants({ variant, size, className }),
@@ -94,5 +96,94 @@ const Button = React.forwardRef<React.ComponentRef<typeof RNPressable>, ButtonPr
 );
 Button.displayName = "Button";
 
-export { Button, buttonTextVariants, buttonVariants };
+type Ripple = { x: number; y: number; id: number };
+const RippleButton = React.forwardRef<
+  React.ComponentRef<typeof RNPressable>,
+  React.ComponentPropsWithoutRef<typeof RNPressable>
+>(({ onPress, children, ...props }, ref) => {
+  const [ripples, setRipples] = React.useState<Ripple[]>([]);
+
+  const handlePress = (e: GestureResponderEvent) => {
+    const id = Date.now();
+    const { offsetX, offsetY, locationX, locationY } =
+      e.nativeEvent as typeof e.nativeEvent & { offsetX: number; offsetY: number };
+    setRipples((r) => [
+      { x: (offsetX ?? locationX) - 50, y: (offsetY ?? locationY) - 50, id },
+    ]);
+    onPress?.(e);
+  };
+
+  return (
+    <RNPressable {...props} ref={ref} onPress={handlePress}>
+      {children as any}
+      {ripples.map(({ x, y, id }) => (
+        <RippleCircle
+          key={id}
+          x={x}
+          y={y}
+          duration={400}
+          onFinish={({ finished }) => {
+            if (finished) {
+              setRipples((r) => r.filter((p) => p.id !== id));
+            }
+          }}
+        />
+      ))}
+    </RNPressable>
+  );
+});
+
+type CircleProps = React.ComponentPropsWithoutRef<typeof Animated.View> & {
+  x: number;
+  y: number;
+  duration: number;
+  onFinish: Animated.EndCallback;
+};
+
+const RippleCircle: React.FC<CircleProps> = ({
+  x,
+  y,
+  onFinish,
+  duration = isWeb ? 600 : 400,
+  className,
+  style,
+  ...props
+}) => {
+  const progress = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    Animated.timing(progress, {
+      toValue: 1,
+      duration,
+      useNativeDriver: !isWeb,
+    }).start(onFinish);
+  }, []);
+
+  const animatedStyle = {
+    top: y,
+    left: x,
+    opacity: progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+    }),
+    transform: [
+      {
+        scale: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+        }),
+      },
+    ],
+  };
+
+  return (
+    <Animated.View
+      pointerEvents="none"
+      className={cn("absolute bg-accent rounded-[50px] h-[100px] w-[100px]", className)}
+      style={[animatedStyle, style]}
+      {...props}
+    />
+  );
+};
+
+export { Button, RippleButton, RippleCircle, buttonTextVariants, buttonVariants };
 export type { ButtonProps };
