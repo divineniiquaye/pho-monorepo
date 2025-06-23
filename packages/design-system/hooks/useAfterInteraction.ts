3b7ddb2983c4863183a06035edfb32a854dfd561
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-06-23 10:26:33 +0000

    feat:(design-system): refactor useAfterInteraction hook
    - Use `useRef` for managing interaction handle and timeout for better cleanup.
    - Ensure safe and consistent cleanup on component unmount or re-render.

--- packages/design-system/hooks/useAfterInteraction.ts
+++ packages/design-system/hooks/useAfterInteraction.ts
@@ -2,7 +2,7 @@ import { InteractionManager } from "react-native";
 import React from "react";
 
 export function useAfterInteractions(
-    delay?: number | false,
+    delay: number | false = false,
     /**
      * Optional async callback that returns a boolean
      * to drive the “ready” state. If you don’t need
@@ -12,29 +12,32 @@ export function useAfterInteractions(
     deps: React.DependencyList = [],
 ) {
     const [ready, setReady] = React.useState(Boolean(process.env.JEST_WORKER_ID));
+    const interactionHandle = React.useRef<ReturnType<
+        typeof InteractionManager.runAfterInteractions
+    > | null>(null);
+    const timeoutRef = React.useRef<ReturnType<typeof setTimeout> | null>(null);
+
     React.useEffect(() => {
         if (process.env.JEST_WORKER_ID) return;
-        let cancel: ReturnType<typeof InteractionManager.runAfterInteractions>;
-        let timeout: ReturnType<typeof setTimeout>;
-        const handler = async () => {
-            const result = callback ? await callback() : true;
-            setReady(result);
+        const runInteraction = async () => {
+            interactionHandle.current = InteractionManager.runAfterInteractions(
+                async () => {
+                    const result = callback ? await callback() : true;
+                    setReady(result);
+                },
+            );
         };
 
-        if (typeof delay === "number") {
-            timeout = setTimeout(
-                async () => {
-                    cancel = await InteractionManager.runAfterInteractions(handler);
-                },
-                Math.max(300, delay),
-            );
-        } else {
-            cancel = InteractionManager.runAfterInteractions(handler);
-        }
+        if (delay === false) runInteraction();
+        else timeoutRef.current = setTimeout(runInteraction, Math.max(delay, 300));
 
         return () => {
-            if (timeout) clearTimeout(timeout);
-            cancel?.cancel();
+            interactionHandle.current?.cancel?.();
+            interactionHandle.current = null;
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+                timeoutRef.current = null;
+            }
         };
     }, [callback, delay, ...deps]);
 
