8096c21ad464f756485dbf961e4059f0abea3c99
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-08-20 12:39:01 +0000

    feat(mobile): update Metro config with enhanced resolver & minifier

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -4,6 +4,7 @@
  * @type {import('expo/metro-config')}
  */
 const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
+// const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 const { FileStore } = require("metro-cache");
@@ -15,7 +16,8 @@ const projectRoot = __dirname;
 // The workspace root is the parent of the project root
 const workspaceRoot = path.resolve(projectRoot, "../..");
 
-const config = getDefaultConfig(projectRoot);
+// If you want to use sentry, replace getDefaultConfig with getSentryExpoConfig
+const config = getDefaultConfig(projectRoot, { isCSSEnabled: true });
 const globalCSS = path.resolve(
     workspaceRoot,
     "packages/design-system/tailwind/global.css",
@@ -31,12 +33,18 @@ config.resolver.nodeModulesPaths = [
 ];
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
-// 4. Enable package exports for metro
+// 4. Configure the resolver with new options
+config.watcher.unstable_lazySha1 = true;
 config.resolver.unstable_enablePackageExports = true;
+config.resolver.unstable_conditionNames = ["react-native", "require", "browser"];
 // 5. This repository is configured Turborepo to use this cache location.
 config.cacheStores = [
     new FileStore({ root: path.join(__dirname, "node_modules/.cache/metro") }),
 ];
+// 6. The option below removes all console logs statements in production.
+config.transformer.minifierConfig = {
+    compress: { drop_console: true },
+};
 
 module.exports = withNativeWind(wrapWithReanimatedMetroConfig(config), {
     configPath: tailwindConfigPath,
