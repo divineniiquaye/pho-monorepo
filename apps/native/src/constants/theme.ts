6831c16bfc4e7cae9322510594cdc87a21113d13
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-08-20 12:40:04 +0000

    feat(mobile): update font & background colors in theme constants

--- apps/native/src/constants/theme.ts
+++ apps/native/src/constants/theme.ts
@@ -1,6 +1,6 @@
 import { DefaultTheme, type Theme } from "@react-navigation/native";
 
-export const DefaultFont = "NunitoSans";
+export const DefaultFont = "Geist";
 export const FontTheme = {
     regular: {
         fontFamily: DefaultFont,
@@ -21,7 +21,7 @@ export const FontTheme = {
 };
 
 export const LightTheme = {
-    background: "transparent", // background
+    background: "hsl(0 0% 100%)", // background
     border: "hsl(240 5.9% 90%)", // border
     card: "hsl(0 0% 100%)", // card
     notification: "hsl(0 84.2% 60.2%)", // destructive
@@ -30,7 +30,7 @@ export const LightTheme = {
 };
 
 export const DarkTheme = {
-    background: "transparent", // background
+    background: "hsl(240 10% 3.9%)", // background
     border: "hsl(240 3.7% 15.9%)", // border
     card: "hsl(240 10% 3.9%)", // card
     notification: "hsl(0 72% 51%)", // destructive
