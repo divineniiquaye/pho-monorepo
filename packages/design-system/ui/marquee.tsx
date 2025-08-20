c4eb5a45a414b6b400dc2c80a92f64bd5891de7e
Divine Niiquaye Ibok <divineibok@gmail.com>
2025-08-20 09:54:15 +0000

    feat(design-system): improved marquee & added seperator

--- packages/design-system/ui/marquee.tsx
+++ packages/design-system/ui/marquee.tsx
@@ -26,6 +26,7 @@ interface TranslatedElementProps {
   children: React.ReactNode;
   offset: Animated.SharedValue<number>;
   childrenWidth: number;
+  separatorWidth: number;
 }
 
 const TranslatedElement: React.FC<TranslatedElementProps> = ({
@@ -33,10 +34,11 @@ const TranslatedElement: React.FC<TranslatedElementProps> = ({
   children,
   offset,
   childrenWidth,
+  separatorWidth,
 }) => {
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      left: (index - 1) * childrenWidth,
+      left: index * (childrenWidth + separatorWidth),
       transform: [
         {
           translateX: -offset.value,
@@ -57,6 +59,8 @@ interface ChildrenScrollerProps {
   parentWidth: number;
   reverse: boolean;
   children: React.ReactNode;
+  separator?: React.ReactNode;
+  separatorWidth: number;
 }
 
 const ChildrenScroller: React.FC<ChildrenScrollerProps> = ({
@@ -65,30 +69,45 @@ const ChildrenScroller: React.FC<ChildrenScrollerProps> = ({
   parentWidth,
   reverse,
   children,
+  separator,
+  separatorWidth,
 }) => {
   const offset = useSharedValue(0);
-  const coeff = useSharedValue(reverse ? 1 : -1);
+  const coeff = useSharedValue(reverse ? -1 : 1);
 
   React.useEffect(() => {
-    coeff.value = reverse ? 1 : -1;
+    coeff.value = reverse ? -1 : 1;
   }, [reverse]);
 
   useFrameCallback((i) => {
+    const totalWidth = childrenWidth + separatorWidth;
     offset.value +=
-      (coeff.value * ((i.timeSincePreviousFrame ?? 1) * childrenWidth)) / duration;
-    offset.value = offset.value % childrenWidth;
+      (coeff.value * ((i.timeSincePreviousFrame ?? 1) * totalWidth)) / duration;
+    offset.value = offset.value % totalWidth;
   }, true);
 
-  const count = Math.round(parentWidth / childrenWidth) + 2;
+  const count = Math.ceil(parentWidth / (childrenWidth + separatorWidth)) + 1;
   const renderChild = (index: number) => (
-    <TranslatedElement
-      key={`clone-${index}`}
-      index={index}
-      offset={offset}
-      childrenWidth={childrenWidth}
-    >
-      {children}
-    </TranslatedElement>
+    <React.Fragment key={`clone-${index}`}>
+      <TranslatedElement
+        index={index}
+        offset={offset}
+        childrenWidth={childrenWidth}
+        separatorWidth={separatorWidth}
+      >
+        {children}
+      </TranslatedElement>
+      {separator && (
+        <TranslatedElement
+          index={index}
+          offset={offset}
+          childrenWidth={childrenWidth}
+          separatorWidth={separatorWidth}
+        >
+          <View style={{ left: childrenWidth }}>{separator}</View>
+        </TranslatedElement>
+      )}
+    </React.Fragment>
   );
 
   return <>{Array.from({ length: count }, (_, i) => i).map(renderChild)}</>;
@@ -97,12 +116,17 @@ const ChildrenScroller: React.FC<ChildrenScrollerProps> = ({
 type MarqueeProps = View["props"] & {
   duration?: number;
   reverse?: boolean;
+  separator?: React.ReactNode;
 };
 
 export const Marquee = React.forwardRef<View, MarqueeProps>(
-  ({ duration = 2000, reverse = false, children, onLayout, ...props }, ref) => {
+  (
+    { duration = 2000, reverse = false, separator, children, onLayout, ...props },
+    ref,
+  ) => {
     const [parentWidth, setParentWidth] = React.useState(0);
     const [childrenWidth, setChildrenWidth] = React.useState(0);
+    const [separatorWidth, setSeparatorWidth] = React.useState(0);
 
     return (
       <View
@@ -114,19 +138,27 @@ export const Marquee = React.forwardRef<View, MarqueeProps>(
         }}
         {...props}
       >
-        <View className="flex-row overflow-hidden" pointerEvents="box-none">
+        <View className="flex-row items-center overflow-hidden" pointerEvents="box-none">
           <MeasureElement onLayout={setChildrenWidth}>{children}</MeasureElement>
 
-          {childrenWidth > 0 && parentWidth > 0 && (
-            <ChildrenScroller
-              duration={duration}
-              parentWidth={parentWidth}
-              childrenWidth={childrenWidth}
-              reverse={reverse}
-            >
-              {children}
-            </ChildrenScroller>
+          {separator && (
+            <MeasureElement onLayout={setSeparatorWidth}>{separator}</MeasureElement>
           )}
+
+          {childrenWidth > 0 &&
+            parentWidth > 0 &&
+            (separator ? separatorWidth > 0 : true) && (
+              <ChildrenScroller
+                duration={duration}
+                parentWidth={parentWidth}
+                childrenWidth={childrenWidth}
+                reverse={reverse}
+                separator={separator}
+                separatorWidth={separator ? separatorWidth : 4}
+              >
+                {children}
+              </ChildrenScroller>
+            )}
         </View>
       </View>
     );
